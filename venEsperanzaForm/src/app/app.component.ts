d0e41147f640ed4cfb4fedda4f2659ca322fe707
JorgeCamargoR <jotas1_4@hotmail.com>
2020-11-12 13:35:00 -0500

    validacion por parametro de identificador de publicidad

--- venEsperanzaForm/src/app/app.component.ts
+++ venEsperanzaForm/src/app/app.component.ts
@@ -13,48 +13,52 @@ export class AppComponent implements OnInit {
 
   title = 'venEsperanzaForm';
   error = false;
-  referrer: any = document.referrer; //origen del trafico
-  queryString: any = window.location.search; //obtener url
+  referrer: any = document.referrer; // origen del trafico
+  queryString: any = window.location.search; // obtener url
   color: ThemePalette = 'primary';
   mode: ProgressSpinnerMode = 'determinate';
   value = 50;
-  show = false; //debe ser false y habilitar el if de validar referrer
+  show = false; // debe ser false y habilitar el if de validar referrer
 
 
   constructor(private formService: FormService) {
   }
 
   ngOnInit() {
+    // console.log('REFERERRR: ', this.referrer);
+    // console.log('LOCATIONSEARCH ', this.queryString);
 
-    //const urlParams = new URLSearchParams(this.queryString);  //url
-    //const fbclid = urlParams.get('fbclid'); //parametro fbclid
-    //this.referrer = 'http://l.facebook.com'; //valor referer para transito facebook
+    const urlParams = new URLSearchParams(this.queryString);  // url
+    // console.log('LOCATIONSEARCH ', urlParams.get('isv'));
+    const isv = urlParams.get('isv'); // parametro isv
+    // const fbclid = urlParams.get('fbclid'); //parametro fbclid
+    // this.referrer = 'http://l.facebook.com'; //valor referer para transito facebook
 
-     //Se debe descomentar para validar fuente de transito, ubicación y ip en back
+     // Se debe descomentar para validar fuente de transito, ubicación y ip en back
     if (this.referrer.includes('http://l.facebook.com') || this.referrer.includes('https://l.facebook.com')
       || this.referrer.includes('http://facebook.com') || this.referrer.includes('https://facebook.com')
       || this.referrer.includes('http://m.facebook.com') || this.referrer.includes('https://m.facebook.com')
       || this.referrer.includes('http://lm.facebook.com') || this.referrer.includes('http://lm.facebook.com')) {
 
-      //validacion de navegacion comentado provisionalmente
+      // validacion de navegacion comentado provisionalmente
       navigator.geolocation.getCurrentPosition((position) => {
-        //geolocalizacion tomada del navegador
-        let coords = {'latitud':position.coords.latitude,'longitud':position.coords.longitude};
-        //geolocalizacion prueba
-        //Cucuta: 7.9116667,-72.5261027
-        //let coords = {'latitud': -72.5261027, 'longitud': 7.9116667};
-        //VRosario:
-        //7.865935,-72.4673127
-        //let coords = {'latitud': -72.466412,'longitud':7.823252  };
-        //let coords = {'latitud': -72.4673127,'longitud':7.8659352};
-        //let coords = {'latitud': -72.478822   ,'longitud':7.576244 }; //por fuera
-        let datos = {'coordenadas': coords};
-        //console.log("SE CARGA ARRAY COORDENADAS?");
+        // geolocalizacion tomada del navegador
+        let coords = {'latitud':position.coords.latitude, 'longitud' : position.coords.longitude};
+        // geolocalizacion prueba
+        // Cucuta: 7.9116667,-72.5261027
+        // let coords = {'latitud': -72.5261027, 'longitud': 7.9116667};
+        // VRosario:
+        // 7.865935,-72.4673127
+        // let coords = {'latitud': -72.466412,'longitud':7.823252  };
+        // let coords = {'latitud': -72.4673127,'longitud':7.8659352};
+        // let coords = {'latitud': -72.478822   ,'longitud':7.576244 }; //por fuera
+        let datos = {'coordenadas': coords, 'adf' : isv};
+        // console.log("SE CARGA ARRAY COORDENADAS?");
         this.formService.validarUbicacionVR(datos).subscribe(res => {
-          //console.log(res);
+          // console.log(res);
           // res['existeenpoligono'] = 1;
-          //console.log(res['existeenpoligono']);
-          if (res['existeenpoligono']) {
+          // console.log(res['existeenpoligono']);
+          if (res['existeenpoligono'] == true) {
             this.show = true;
           } else {
             this.error = true;
@@ -64,11 +68,10 @@ export class AppComponent implements OnInit {
           this.error = true;
         });
       }, error => {
-        this.error = true; //muestra error xq no habilita geolocalizacion
+        this.error = true; // muestra error xq no habilita geolocalizacion
       });
-      
     } else {
-      this.error = true; //muestra error porq el trafico no proviene de facebook
+      this.error = true; // muestra error porq el trafico no proviene de facebook
     }
   }
 
