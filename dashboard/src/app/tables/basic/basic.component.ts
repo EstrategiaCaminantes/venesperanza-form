53e4e134b6fc8aeeb4af74f4fbe72e9cd33f2d0f
Sebastian Navarrete <sebasnavarrete.2@gmail.com>
2020-12-09 12:14:41 -0500

    dash v1.0

--- dashboard/src/app/tables/basic/basic.component.ts
+++ dashboard/src/app/tables/basic/basic.component.ts
@@ -1,88 +1,95 @@
 import {Component, OnInit, ViewChild} from '@angular/core';
 import {EncuestaService} from '../../services/encuesta.service';
-import { MessageService } from '../../@pages/components/message/message.service';
-import { AuthService } from '../../services/auth.service';
+import {MessageService} from '../../@pages/components/message/message.service';
+import {AuthService} from '../../services/auth.service';
 
 @Component({
-    selector: 'app-basic',
-    templateUrl: './basic.component.html',
-    styleUrls: ['./basic.component.scss']
+  selector: 'app-basic',
+  templateUrl: './basic.component.html',
+  styleUrls: ['./basic.component.scss']
 })
 export class BasicComponent implements OnInit {
-    encuestas;
+  encuestas = [];
 
-    sortName = null;
-    sortValue = null;
-    selected = [];
-    scrollBarHorizontal = window.innerWidth < 960;
-    columnModeSetting = window.innerWidth < 960 ? 'standard' : 'force';
-    columnModeSettingSmall = window.innerWidth < 560 ? 'standard' : 'force';
+  sortName = null;
+  sortValue = null;
+  meta = 0;
+  selected = [];
+  scrollBarHorizontal = window.innerWidth < 960;
+  columnModeSetting = window.innerWidth < 960 ? 'standard' : 'force';
+  columnModeSettingSmall = window.innerWidth < 560 ? 'standard' : 'force';
 
-    constructor(private encuesta: EncuestaService, private messageservice:  MessageService, private authService: AuthService) {
-        window.onresize = () => {
-            this.scrollBarHorizontal = window.innerWidth < 960;
-            this.columnModeSetting = window.innerWidth < 960 ? 'standard' : 'force';
-            this.columnModeSettingSmall = window.innerWidth < 560 ? 'standard' : 'force';
-        };
-    }
+  constructor(private encuesta: EncuestaService, private messageservice: MessageService, private authService: AuthService) {
+    window.onresize = () => {
+      this.scrollBarHorizontal = window.innerWidth < 960;
+      this.columnModeSetting = window.innerWidth < 960 ? 'standard' : 'force';
+      this.columnModeSettingSmall = window.innerWidth < 560 ? 'standard' : 'force';
+    };
+  }
 
-    ngOnInit() {
-        this.encuesta.getData().subscribe((data: any[]) => {
-                this.encuestas = data;
-        }, error => {
-            //console.log('ERROR: ', error);
-            if (error['error']['message'] === 'Unauthenticated') {
-                this.messageservice.create(
-                    'danger',
-                    'No autorizado!',
-                    {
-                    Position: 'top-right',
-                    Style: 'bar',
-                    Duration: 3000
-                    }
-                );
-               this.authService.logout();
-            } else {
-                this.messageservice.create(
-                    'danger',
-                    'Error en servidor, intenta de nuevo.',
-                    {
-                    Position: 'top-right',
-                    Style: 'bar',
-                    Duration: 3000
-                    }
-                );
-            }
-        });
-    }
+  ngOnInit() {
+    this.encuesta.getData().subscribe((data: any[]) => {
+      this.encuestas = data;
+      this.meta = 0;
+      data.forEach((obj: any) => {
+        if (obj.puntaje >= 9) {
+          this.meta++;
+        }
+      });
+    }, error => {
+      //console.log('ERROR: ', error);
+      if (error['error']['message'] === 'Unauthenticated') {
+        this.messageservice.create(
+          'danger',
+          'No autorizado!',
+          {
+            Position: 'top-right',
+            Style: 'bar',
+            Duration: 3000
+          }
+        );
+        this.authService.logout();
+      } else {
+        this.messageservice.create(
+          'danger',
+          'Error en servidor, intenta de nuevo.',
+          {
+            Position: 'top-right',
+            Style: 'bar',
+            Duration: 3000
+          }
+        );
+      }
+    });
+  }
 
-    onSelect({selected}) {
-        this.selected.splice(0, this.selected.length);
-        this.selected.push(...selected);
-    }
+  onSelect({selected}) {
+    this.selected.splice(0, this.selected.length);
+    this.selected.push(...selected);
+  }
 
-    onActivate(event) {
-    }
+  onActivate(event) {
+  }
 
-    fetch(cb) {
-        const req = new XMLHttpRequest();
-        req.open('GET', `assets/data/table.json`);
+  fetch(cb) {
+    const req = new XMLHttpRequest();
+    req.open('GET', `assets/data/table.json`);
 
-        req.onload = () => {
-            cb(JSON.parse(req.response));
-        };
+    req.onload = () => {
+      cb(JSON.parse(req.response));
+    };
 
-        req.send();
-    }
+    req.send();
+  }
 
-    //Expandable Table Code;
-    @ViewChild('expTable', {static: true}) expTable: any;
-    expanded: any = {};
+  //Expandable Table Code;
+  @ViewChild('expTable', {static: true}) expTable: any;
+  expanded: any = {};
 
-    toggleExpandRow(row) {
-        this.expTable.rowDetail.toggleExpandRow(row);
-    }
+  toggleExpandRow(row) {
+    this.expTable.rowDetail.toggleExpandRow(row);
+  }
 
-    onDetailToggle(event) {
-    }
+  onDetailToggle(event) {
+  }
 }
