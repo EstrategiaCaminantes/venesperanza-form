633ab0081fc8a127384da663932f74d81f489103
Sebastian Navarrete <sebasnavarrete.2@gmail.com>
2021-03-04 16:28:58 -0500

    btn cancelar

--- venEsperanzaForm/src/app/home/home.component.ts
+++ venEsperanzaForm/src/app/home/home.component.ts
@@ -1,6 +1,7 @@
 import {Component, ElementRef, Input, OnInit, ViewChild} from '@angular/core';
 import {FormGroup, FormControl, FormBuilder, Validators, FormArray} from '@angular/forms';
-import { Router, ActivatedRoute, ParamMap } from '@angular/router';
+import {Router, ActivatedRoute, ParamMap} from '@angular/router';
+
 //import { requiredFileType } from "./requireFileTypeValidators";
 
 
@@ -11,10 +12,9 @@ import { Router, ActivatedRoute, ParamMap } from '@angular/router';
 })
 export class HomeComponent implements OnInit {
 
-  
 
-  constructor(private formBuilder: FormBuilder,private elementRef: ElementRef, 
-    private router: Router, private activatedRouter:ActivatedRoute) {
+  constructor(private formBuilder: FormBuilder, private elementRef: ElementRef,
+              private router: Router, private activatedRouter: ActivatedRoute) {
   }
 
   ngOnInit(): void {
